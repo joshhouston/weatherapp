724918058ef73869d4c8447a097268a571e05b53
josh <jjhouston24@gmail.com>
2021-01-11 09:41:24 -0600

    optimized code by using for loop

--- src/components/FourDay.js
+++ src/components/FourDay.js
@@ -5,16 +5,10 @@ class FourDay extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            todayMin: '',
-            todayMax: '',
-            tomorrowMin: '',
-            tomorrowMax: '',
-            thirdMin: '',
-            thirdMax: '',
-            thirdDate: '',
-            fourthMin: '',
-            fourthMax: '',
-            fourthDate: ''
+            minTemp: [],
+            maxTemp: [],
+            icon: [],
+            dates: []
         }
     }
 
@@ -22,60 +16,58 @@ class FourDay extends Component {
         axios
             .get(`https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=minutely,hourly&appid=14591153586b9b9f00539b13c8a274a1`)
             .then(response => {
-                console.log(response.data.daily[2].dt)
+                const minMax = response.data.daily.slice(0,4);
+                const minTemp = [];
+                const maxTemp = [];
+                const icon = [];
+                const dates = [];
+                
+                
+                for(let i=0; i < minMax.length; i++){
 
-                //Third Day Date xoxo
-                let dt3 = response.data.daily[2].dt,
-                    date3 = new Date(dt3 * 1000).toString().substring(0,4),
-                    dt4 = response.data.daily[3].dt,
-                    date4 = new Date(dt4 * 1000).toString().substring(0,4)
+                    //Get Date
+                    let dt = minMax[i].dt;
+                    let date = new Date(dt * 1000).toString().substring(0,4);
+                    dates.push(date)
+                    
+                    //Get Min & Max
+                    let min = Math.floor(minMax[i].temp.min * 9/5 - 459.67);
+                    console.log(min)
+                    minTemp.push(min)
+                    let max = Math.floor(minMax[i].temp.max * 9/5 - 459.67);
+                    maxTemp.push(max)
 
-                // Min & Max Temps
-                let todayMin = Math.floor(response.data.daily[0].temp.min * 9/5 - 459.67),
-                    todayMax = Math.floor(response.data.daily[0].temp.max * 9/5 - 459.67),
-                    tomorrowMin = Math.floor(response.data.daily[1].temp.min * 9/5 - 459.67),
-                    tomorrowMax = Math.floor(response.data.daily[1].temp.max * 9/5 - 459.67),
-                    thirdMin = Math.floor(response.data.daily[2].temp.min * 9/5 - 459.67),
-                    thirdMax = Math.floor(response.data.daily[2].temp.max * 9/5 - 459.67),
-                    fourthMin = Math.floor(response.data.daily[3].temp.min * 9/5 - 459.67),
-                    fourthMax = Math.floor(response.data.daily[3].temp.max * 9/5 - 459.67)
-
+                    //Get Weather Icon status
+                    let icons = minMax[i].weather[0].icon;
+                    icon.push(icons)
+                }
                 
                 this.setState({
-                    todayMin: todayMin,
-                    todayMax: todayMax,
-                    tomorrowMin: tomorrowMin,
-                    tomorrowMax: tomorrowMax,
-                    thirdMin: thirdMin,
-                    thirdMax: thirdMax,
-                    thirdDate: date3,
-                    fourthMin: fourthMin,
-                    fourthMax: fourthMax,
-                    fourthDate: date4
+                    dates: dates,
+                    minTemp: minTemp,
+                    maxTemp: maxTemp,
+                    icon: icon
+
                 })
                  
             })
     }
 
+    
+
     render(){
         return (
             <div className="main">
-                <div className="today">
-                    Today
-                    <p>{this.state.todayMin}-{this.state.todayMax}</p>
-                </div>
-                <div className="tomorrow">
-                    Tomorrow
-                    <p>{this.state.tomorrowMin}-{this.state.tomorrowMax}</p>
-                </div>
-                <div className="third-day">
-                    {this.state.thirdDate}
-                    <p>{this.state.thirdMin}-{this.state.thirdMax}</p>
-                </div>
-                <div className="fourth-day">
-                    {this.state.fourthDate}
-                    <p>{this.state.fourthMin}-{this.state.fourthMax}</p>
-                </div>
+
+                {this.state.dates.map((date, index) => {
+                    return (
+                        <div className="date" key={index}>
+                            <img src={`http://openweathermap.org/img/wn/${this.state.icon[index]}.png`} alt="Weather Icon"/>
+                            <p>{date}</p>
+                            <p>{this.state.minTemp[index]}-{this.state.maxTemp[index]}</p>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
